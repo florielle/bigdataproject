1b5ab36205aa669847f4046fadefcdd9bbf68751
Felipe Ducau <fnd212@nyu.edu>
2017-04-08 14:38:48 -0400

    Update countuniques.py

--- countuniques.py
+++ countuniques.py
@@ -9,6 +9,13 @@ if __name__ == "__main__":
     sc = SparkContext()
 
     lines = sc.textFile(sys.argv[1], 1)
+       first_line = lines.first()
+
+    if first_line.split(',')[0] == u'CMPLNT_NUM':
+        # First line is header
+        # Filter header out
+        lines = lines.filter(lambda x: x != first_line)
+
 
     lines = lines.mapPartitions(lambda x: reader(x))\
     .map(lambda x: (x[column],1))\
