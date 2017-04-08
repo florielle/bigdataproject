a2e77b6093b807f822ce2ac825f94a8342290f26
florielle <jmw784@nyu.edu>
2017-04-08 15:10:42 -0400

    Modifying delay.py to include standard deviation calculation

--- delay.py
+++ delay.py
@@ -18,10 +18,16 @@ if __name__ == "__main__":
 
     lines = lines.mapPartitions(lambda x: reader(x))\
     .map(lambda x: ((x[7], (delta_days(x[1], x[5]), 1))))\
-    .filter(lambda x: x[1][0] != '')\
-    .reduceByKey(lambda x,y: (x[0]+y[0], x[1]+y[1]))\
-    .map(lambda x: (x[0], float(x[1][0])/x[1][1]))
+    .filter(lambda x: x[1][0] != '')
+    
+    avg_N = lines.reduceByKey(lambda x,y: (x[0]+y[0], x[1]+y[1]))\
+    .map(lambda x: (x[0], (float(x[1][0])/x[1][1], x[1][1])))\
+    .collectAsMap()
 
-    lines.saveAsTextFile("output.out")
+    output = lines.map(lambda x: (x[0], (x[1][0] - avg_N[x[0]][0])**2))\
+    .reduceByKey(lambda x,y: x+y)\
+    .map(lambda x: (x[0], avg_N[x[0]][0], (x[1]/float(avg_N[x[0]][1]))**(0.5)))
+    
+    output.saveAsTextFile("output.out")
 
     sc.stop()
