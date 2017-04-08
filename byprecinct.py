36fc0fbbb5fb4b23bc9d1da9eb597216ff7f66fa
fducau <fnd212@nyu.edu>
2017-04-08 19:39:27 -0400

    Renaming

--- byprecinct.py
+++ byprecinct.py
@@ -9,10 +9,11 @@ if __name__ == "__main__":
     lines = sc.textFile(sys.argv[1], 1)
 
     lines = lines.mapPartitions(lambda x: reader(x))\
+    .filter(lambda x: x != '')\
     .map(lambda x: ((x[14]),1))\
     .reduceByKey(lambda x,y: x+y)\
     .sortByKey()
 
-    lines.saveAsTextFile("by_county.out")
+    lines.saveAsTextFile("by_prescinct.out")
 
     sc.stop()
