20022fc4702a9ba237b3a71d87c5b15030253e66
fducau <fnd212@nyu.edu>
2017-04-08 12:58:48 -0400

    Checking for header

--- submission/col0.py
+++ submission/col0.py
@@ -8,6 +8,13 @@ if __name__ == "__main__":
     sc = SparkContext()
 
     lines = sc.textFile(sys.argv[1], 1)
+    first_line = lines.first()
+
+    if first_line.split(',')[0] == u'CMPLNT_NUM':
+        # First line is header
+        # Filter header out
+        lines = lines.filter(lambda x: x != first_line)
+
 
     def valid_digit(num):
         if num.isdigit():
