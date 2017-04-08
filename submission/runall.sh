36dedbd01c9470289f1c04c9fa2ff4fb2c180a93
fducau <fnd212@nyu.edu>
2017-04-08 13:51:58 -0400

    Echo command corrected

--- submission/runall.sh
+++ submission/runall.sh
@@ -2,14 +2,14 @@ rm -r -f output
 mkdir output
 
 for I in {1..22}
-    echo Removing output files from hfs 
+    echo "Removing output files from hfs"
     hfs -rm -r -skipTrash col$I.out
 
 for PYFILE in $ls *.py
-    echo running $PYFILE
+    echo "running $PYFILE"
     spark-submit ./$PYFILE NYPD_Complaint_Data_Historic
 done
 
 for I in {1..22}
-    echo Retrieving output files from hfs 
+    echo "Retrieving output files from hfs"
     hfs -getmerge col$I.out col$I.out 
