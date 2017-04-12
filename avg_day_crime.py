a4481dea560d0b81f09730ba03db9ab7595d62f5
Alexandra M Simonoff <ams889@login-1-1.local>
2017-04-12 01:06:10 -0400

    adds in daily and avg day crime counts and avgs

---
+++ avg_day_crime.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+import sys
+from pyspark import SparkContext
+from csv import reader
+from datetime import datetime
+
+if __name__ == "__main__":
+    sc = SparkContext()
+
+    lines = sc.textFile(sys.argv[1], 1)
+    def day_of_crime(day):
+        try:
+            crime_date = datetime.strptime(day,'%m/%d/%Y')
+            year_crime = crime_date.year
+            monthday = ''.join([str(crime_date.month),'-',str(crime_date.day)])
+            if year_crime>= 2007:
+              return monthday
+            else:
+                return ''
+        except:
+            return ''
+
+    lines = lines.mapPartitions(lambda x: reader(x))\
+    .map(lambda x: (x[1], 1))\
+    .reduceByKey(lambda x,y: x+y)\
+    .map(lambda x: (day_of_crime(x[0]), (x[1], 1)))\
+    .reduceByKey(lambda x,y: (x[0]+y[0], x[1]+y[1]))\
+    .map(lambda x: (x[0], float(x[1][0])/x[1][1]))
+
+    lines.saveAsTextFile("avg_day_crimes.out")
+
+    sc.stop()
+
