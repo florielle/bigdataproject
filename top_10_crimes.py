f5f5945d26567213eda802da558a0437afa565e6
Alexandra M Simonoff <ams889@login-2-1.local>
2017-04-12 20:42:18 -0400

    top 10 crime file create

---
+++ top_10_crimes.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+import sys
+from pyspark import SparkContext
+from csv import reader
+from datetime import datetime
+
+if __name__ == "__main__":
+    sc = SparkContext()
+
+    lines = sc.textFile(sys.argv[1], 1)
+
+    lines = lines.mapPartitions(lambda x: reader(x))\
+    .map(lambda x: (x[9], 1))\
+    .reduceByKey(lambda x,y: x+y)\
+    .sortBy(lambda x: -x[1])
+
+    sc.parallelize(lines.take(10))\
+    .saveAsTextFile("top_10_crimes.out")
+
+    sc.stop()
+
