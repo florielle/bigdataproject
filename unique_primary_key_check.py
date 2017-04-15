df7a7c37e6b708740e16666977572411d337e924
Alexandra M Simonoff <ams889@login-1-1.local>
2017-04-15 12:47:02 -0400

    primary key dupe check

---
+++ unique_primary_key_check.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+import sys
+from pyspark import SparkContext
+from csv import reader
+from datetime import datetime
+
+if __name__ == "__main__":
+    sc = SparkContext()
+
+    lines = sc.textFile(sys.argv[1], 1)
+
+    lines = lines.mapPartitions(lambda x: reader(x))\
+    .map(lambda x: (x[0], 1))\
+    .reduceByKey(lambda x,y: x+y)\
+    .filter(lambda x: (x[1]>1))
+
+    lines.saveAsTextFile("duplicate_keys.out")
+
+    sc.stop()
+
