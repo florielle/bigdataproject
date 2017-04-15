242929d061641b42dfe030652b22c9872951926b
fducau <fnd212@nyu.edu>
2017-04-15 15:19:01 -0400

    by precinct only violent crimes

---
+++ byprecinct_violent.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+import sys
+from pyspark import SparkContext
+from csv import reader
+
+
+
+if __name__ == "__main__":
+    sc = SparkContext()
+
+    lines = sc.textFile(sys.argv[1], 1)
+
+    lines = lines.mapPartitions(lambda x: reader(x))\
+    .filter(lambda x: x != '')\
+    .filter(lambda x: ('HOMICIDE' in x[7] or 'MURDER' in x[7] or 'ASSAULT' in x[7] or 'RAPE' in x[7] or 'ROBBERY' in x[7]))\
+    .map(lambda x: ((x[14]),1))\
+    .reduceByKey(lambda x,y: x+y)\
+    .sortByKey()
+
+    lines.saveAsTextFile("by_prescinct.out")
+
+    sc.stop()
