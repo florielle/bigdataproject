d24a9cd3cbdf0fe6896d53c06f058215d8f44f1a
Alexandra M Simonoff <ams889@login-2-1.local>
2017-04-15 02:50:45 -0400

    monthly counts for projection

---
+++ monthly_crime.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+import sys
+from pyspark import SparkContext
+from csv import reader
+from datetime import datetime
+
+if __name__ == "__main__":
+    sc = SparkContext()
+
+    lines = sc.textFile(sys.argv[1], 1)
+    def month_year(day):
+        try:
+            m=datetime.strptime(day,'%m/%d/%Y').month
+            y=datetime.strptime(day,'%m/%d/%Y').year
+            if y >= 2008:
+                return str(m)+"-"+str(y)
+            else:
+                return ''
+        except:
+            return ''
+
+    lines = lines.mapPartitions(lambda x: reader(x))\
+    .map(lambda x: (month_year(x[1]), 1))\
+    .reduceByKey(lambda x,y: x+y)\
+    .sortByKey(ascending=True)
+
+    lines.saveAsTextFile("monthly_crime.out")
+
+    sc.stop()
+
